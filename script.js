ff26c8a4360b6ecb88f8aef2e3ff833df033e791
plzDontTouchMe_PC <mikk1m0use@mail.ru>
2023-12-03 02:08:24 +0300

    Сделана генерация ключей) Кайф!

--- script.js
+++ script.js
@@ -1,36 +1,131 @@
 const fs = require('fs');
 class TwoFish{
-    #arrayKeys = [];
     #key;
     #k;
     #r = 16;
+    #arraySubKeys = new Array(40);
     constructor(key, k) {
         this.#k = k;
-        this.#key = key
+        if(key.length > 32){
+            key = key.slice(0, 32);
+        }
+        this.#key = key;
         this.generationSubKeys(key);
     }
     generationSubKeys(key){
-        let M_e = '';
-        let M_0 = '';
+        let M_e = [];
+        let M_0 = [];
         for (let i = 0; i < key.length; i += 8) {
-            M_e += this.getByteByText(key.charCodeAt(i), 8) + this.getByteByText(key.charCodeAt(i + 1), 8) + this.getByteByText(key.charCodeAt(i + 2), 8) + this.getByteByText(key.charCodeAt(i + 3), 8);
-            M_0 += this.getByteByText(key.charCodeAt(i + 4), 8) + this.getByteByText(key.charCodeAt(i + 5), 8) + this.getByteByText(key.charCodeAt(i + 6), 8) + this.getByteByText(key.charCodeAt(i + 7), 8);
+            M_e.push(this.getByteByValue(key.charCodeAt(i), 8) + this.getByteByValue(key.charCodeAt(i + 1), 8) + this.getByteByValue(key.charCodeAt(i + 2), 8) + this.getByteByValue(key.charCodeAt(i + 3), 8));
+            M_0.push(this.getByteByValue(key.charCodeAt(i + 4), 8) + this.getByteByValue(key.charCodeAt(i + 5), 8) + this.getByteByValue(key.charCodeAt(i + 6), 8) + this.getByteByValue(key.charCodeAt(i + 7), 8));
         }
-        for (let i = 0, p = Math.pow(2, 24) + Math.pow(2, 16) + Math.pow(2, 8) + 1; i < this.#r; i++){ //2^25
+        for (let i = 0, p = 2**24 + 2**16 + 2**8 + 1; i < (this.#r + 4) * 2; i+=2){ //2^25
             let A = this.h(2 * i * p, M_e); //2^30
-            //let B = this.ROL(this.h((2 * i + 1) * p, M_0), 8);
+            let B = this.getValueByByte(this.ROL(this.getByteByValue(this.h((2 * i + 1) * p, M_0), 32), 8), 32);
+            this.#arraySubKeys[i] = this.getByteByValue((A + B) % 2**32, 32);
+            this.#arraySubKeys[i + 1] = this.ROL(this.getByteByValue((A + B) % 2**32, 32), 9);
         }
     }
     h(value, subKeyByte){
-        let count = parseFloat(subKeyByte.length) / 64 * 2;
-        let tempArray = new Array(count + 1);
-        for(let i = 0; i < tempArray.length; i++){
-            //tempArray[i] =
+        let q0 = [
+            [8, 1, 7, 13, 6, 15, 3, 2, 0, 11, 5, 9, 14, 12, 10, 4],
+            [14, 12, 11, 8, 1, 2, 3, 5, 15, 4, 10, 6, 7, 0, 9, 13],
+            [11, 10, 5, 14, 6, 13, 9, 0, 12, 8, 15, 3, 2, 4, 7, 1],
+            [13, 7, 15, 4, 1, 2, 6, 14, 9, 11, 3, 0, 8, 5, 12, 10 ]
+        ]
+        let q1 = [
+            [ 2, 8, 11, 13, 15, 7, 6, 14, 3, 1, 9, 4, 0, 10, 12, 5 ],
+            [ 1, 14, 2, 11, 4, 12, 3, 7, 6, 13, 10, 5, 15, 9, 0, 8 ],
+            [ 4, 12, 7, 5, 1, 6, 9, 10, 0, 14, 13, 8, 2, 11, 3, 15 ],
+            [ 11, 9, 5, 1, 12, 3, 13, 14, 6, 4, 7, 15, 2, 0, 8, 10 ]
+        ]
+        let q = [
+            [ 1, 0, 0, 1],
+            [ 1, 1, 0, 0],
+            [ 0, 1, 0, 1],
+            [ 0, 0, 1, 1],
+            [ 1, 0, 1, 0]
+        ]
+        let MDS = [
+            [1, 239, 91, 91],
+            [91, 239, 239, 1],
+            [239, 91, 1, 239],
+            [239, 1, 239, 91]
+        ]
+        if(subKeyByte.length < 4){
+            q.shift();
+            if(subKeyByte.length < 3) {
+                q.shift();
+            }
         }
-        for (let i = 0; i < count; i++){
+        for (let i = 0, indexM = subKeyByte.length - 1; i <= subKeyByte.length; i++){
+            let valueByte = this.getByteByValue(value, 32);
+            let tempQ = new Array(4);
+            let qByte = '';
+            for(let j = 0; j < 4; j++){
+                tempQ[j] = valueByte.slice(j * 8, (j + 1) * 8);
+                let y = q[i][j] === 1 ? this.permutation(tempQ[j], q1) : this.permutation(tempQ[j], q0);
+                qByte += this.getByteByValue(y, 8);
+            }
+            if(i === subKeyByte.length){
+                let tempArray = new Array(4)
+                for(let j = 0; j < 4; j++){
+                    tempArray[j] = this.getValueByByte(qByte.slice(j * 8, (j + 1) * 8), 8);
+                }
+                tempArray = this.multiplyVectorByMatrix(tempArray, MDS);
+                let result = 0;
+                for(let j = 0; j < 4; j++){
+                    result += tempArray[j];
+                }
+                return result;
+            }
+            let tempByte = '';
+            for(let j = 0; j < qByte.length; j++){
+                tempByte += qByte[j] ^ subKeyByte[indexM][j];
+            }
+            value = this.getValueByByte(tempByte, 32);
         }
     }
-    getByteByText(text, degree){
+    multiplyVectorByMatrix(vector, matrix) {
+        var result = [];
+        var rows = matrix.length;
+        var cols = matrix[0].length;
+        if (vector.length !== rows) {
+            console.error("Невозможно умножить вектор на матрицу: количество элементов вектора не соответствует количеству строк матрицы.");
+            return;
+        }
+        for (var i = 0; i < cols; i++) {
+            var sum = 0;
+            for (var j = 0; j < rows; j++) {
+                sum += vector[j] * matrix[j][i];
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+    permutation(x, qTable){
+        let value = this.getValueByByte(x, 8);
+        let a0 = x / 16;
+        let b0 = x % 16;
+        let a1 = a0 ^ b0;
+        let b1 = a0 ^ this.getValueByByte(this.ROR(this.getByteByValue(b0), 1)) ^ ((8 * a0) % 16);
+        let a2 = qTable[0][a1 % 8];
+        let b2 = qTable[1][b1 % 8];
+        let a3 = a2 ^ b2;
+        let b3 = a2 ^ this.getValueByByte(this.ROR(this.getByteByValue(b2), 1)) ^ ((8 * a2) % 16);
+        let a4 = qTable[2][a3 % 8];
+        let b4 = qTable[3][b3 % 8];
+        return 16 * b4 + a4;
+    }
+    ROR(byte, shiftCount){
+        byte = byte.slice(byte.length - shiftCount, byte.length) + byte.slice(0, byte.length - shiftCount)
+        return byte;
+    }
+    ROL(byte, shiftCount){
+        byte = byte.slice(shiftCount, byte.length) + byte.slice(0, shiftCount)
+        return byte;
+    }
+    getByteByValue(text, degree){
         let temp = '';
         for(let i = Math.pow(2, degree - 1); i >= 1; i /= 2){
             if(text - i >= 0){
@@ -43,7 +138,7 @@ class TwoFish{
         }
         return temp;
     }
-    getTextByByte(text, degree){
+    getValueByByte(text, degree){
         let temp = 0;
         let index = 0;
         for(let i = Math.pow(2, degree - 1); i >= 1; i /= 2){
@@ -60,7 +155,7 @@ class TwoFish{
         for(let i = 0; i < arrayDataBlock.length; i++){
             let temp = '';
             for(let j = 0; j < arrayDataBlock[i].length; j++){
-                temp += this.getByteByText(arrayDataBlock[i].charCodeAt(j), 8) + ' ';
+                temp += this.getByteByValue(arrayDataBlock[i].charCodeAt(j), 8) + ' ';
                 //console.log(String.fromCharCode(arrayDataBlock[i].charCodeAt(j)))
             }
             this.splitDataBlock(temp);
